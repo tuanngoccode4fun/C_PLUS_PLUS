79b0295a99125198bde56e1fe9086cef0aa157e3
Admin <Admin@ADMIN>
2022-07-09 22:13:26 +0700

    - the preprocessor

---
+++ 01_Structure of a c++ program/main.cpp
@@ -0,0 +1,21 @@
+/*
+    what is a preprocessor?
+    what does it do?
+    Directive start with '#'
+    Command to the preprocessor
+    #include <iostream>
+    #include "myfile.h"
+#if
+#elif
+#else
+#endif
+#ifdef
+#ifndef
+#define
+#undef
+
+#line
+#error
+#pragma
+*/
+
