910db76574dcba84d3fe165fb7ffa239dc1dae1d
Admin <Admin@ADMIN>
2022-07-09 21:50:05 +0700

    - The reference is only apply for l-value and error when it's an r-value.

--- 00_What is reference/main.cpp
+++ 00_What is reference/main.cpp
@@ -4,22 +4,17 @@
 using namespace std;
 int main()
 {
-  int num =100;
-  int num_2 =500;
-  int &ref= num;
+    /*
+    l-value is addressable if they're not a constant.
+    r-value is not l-value (non-addressable and non-assignable).
+     . A value that's not an l-value.
+        + On the right hand side of an assignment expression.
+        + a literal.
+        + a temporary which is intended to be non-modifiable.
+    */
+   int x = 100;
+   int &ref = x;// ref1 is reference to l-value
+   ref = 1000;
 
-  cout<<"num = "<<num<<endl;
-  cout<<"ref = "<<ref<<endl;
-
-    num =200;
-
-  cout<<"num = "<<num<<endl;
-  cout<<"ref = "<<ref<<endl;
-
-    ref=300;
-
-
-  cout<<"num = "<<num<<endl;
-  cout<<"ref = "<<ref<<endl;
-
+   int &ref_1 = 100; // Error 100 is an r-value.
 }
